87dab6896a1ca5a7a0b6e09b875e620e2c7abb6d
gabrielvsc <gabriel.cesario@ccc.ufcg.edu.br>
2025-05-21 18:05:59 -0300

    feat: create ModeToggle component for switching theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <header>
+    <div className="m-4">
+      <ModeToggle/>
       <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal" />
+        <SignInButton mode="modal" >
+          <Button>Sign In</Button>
+        </SignInButton>
+        <SignUpButton mode="modal" >
+          <Button variant={"secondary"}>Sign Up</Button>
+        </SignUpButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </header>
+    </div>
   );
 }
