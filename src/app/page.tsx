ae1aaa136ca89478c724161dfdee8b2b54f54808
gabrielvsc <gabriel.cesario@ccc.ufcg.edu.br>
2025-05-22 18:56:23 -0300

    feat(layout): add Navbar component and include it in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <ModeToggle/>
-      <SignedOut>
-        <SignInButton mode="modal" >
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton mode="modal" >
-          <Button variant={"secondary"}>Sign Up</Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      Home page
     </div>
   );
 }
